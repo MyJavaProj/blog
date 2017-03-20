a97b162b68a6db98e26bfe67463400c33237527d
龍情雪心 <qianjianlei@163.com>
2017-03-20 13:31:23 +0800

    动态代理

--- src/test/java/com/blog/common/dynamicProxy.java
+++ src/test/java/com/blog/common/dynamicProxy.java
@@ -18,10 +18,6 @@ public class dynamicProxy extends BaseTest{
 	@Test
 	public void dynamicProxyTest(){
 	        RealSubject realSubject = new RealSubject();    //1.创建委托对象
-//	        ProxyHandler handler = new ProxyHandler(realSubject);    //2.创建调用处理器对象
-//	        Subject proxySubject = (Subject)Proxy.newProxyInstance(RealSubject.class.getClassLoader(),
-//	                                                        RealSubject.class.getInterfaces(), handler);    //3.动态生成代理对象
-	        
 	        Subject proxySubject = (Subject)new ProxyHandler().bind(realSubject);
 	        proxySubject.request();    //4.通过代理对象调用方法
 	    }
